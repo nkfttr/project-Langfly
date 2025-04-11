7a5ab9189e9fd1e3553b9db5265cd3c363a604d3
Nico Fatori <nfatorinho@gmail.com>
2025-04-11 02:38:21 -0300

    Utilização de elementos DOM e SVG para os elementos e organizalção HTML
    player: criado como um circulo usando SVG
    
    estrelas no mapa: mapeados como elementos SVG: x, y, font, font-size, fill, class
    
    Alterações no código css: MapaStyle

---
+++ mapCode.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // ELEMENTOS
+    const player = document.getElementById('player');
+    const phaseElements = document.querySelectorAll('.phase');
+
+    // INDICE INICIAL DA FASE NO ARRAY
+    let currentPhase = 0;
+
+    // POSIÇOES NAS FASES SVG
+    const phasePositions = Array.from(phaseElements).map(phase => {
+        // VERIFICA O ELEMNTO, NO CASO TEXT
+        if (phase.tagName === 'text') {
+            return {
+                x: parseFloat(phase.getAttribute('x')),
+                y: parseFloat(phase.getAttribute('y'))
+            };
+        }
+    });
+
+    // ATUAIZAÇÃO DA POSIÇÃO DO PLAYER
+    function updatePlayerPosition() {
+        // Atualiza a posição para a fase atual
+        const targetPos = phasePositions[currentPhase];
+        // VERIFICA A TAG DO JOGADOR, SENDO UM CIRCULO
+        if (player.tagName === 'circle') {
+            // POSIÇÕES DO JOGADOR EM RELAÇÃO ÀS FASES
+            player.setAttribute('cx', targetPos.x+20);
+            player.setAttribute('cy', targetPos.y-5);
+        }
+
+        // ATUALIZAÇÃO DE COORDENADAS
+        coords.textContent = `X: ${Math.round(targetPos.x)}`;
+        coordsY.textContent = `Y: ${Math.round(targetPos.y)}`;
+    }
+
+    // POSICÃO DO JOGADOR
+    updatePlayerPosition();
+
+    document.addEventListener('keydown', (event) => {
+        // DIREITA
+        if (event.key === 'd' || event.key === 'D') {
+            //MOVE O PLAYER NO ARRAY
+            if (currentPhase < phaseElements.length - 1) {
+                currentPhase++;
+                updatePlayerPosition();
+            }
+        }
+        // ESQUERDA
+        else if (event.key === 'a' || event.key === 'A') {
+            //MOVE O PLAYER NO ARRAY
+            if (currentPhase > 0) {
+                currentPhase--;
+                updatePlayerPosition();
+            }
+        }
+
+        // MUDA PRA FASE1
+        if (currentPhase === 1 && event.key === 'k') {
+            window.location.href = "fase1.html";
+        }
+    });
+});
